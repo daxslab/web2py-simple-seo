67ae710e4944f73337150c47a20c4c7db3086d73
Carlos Cesar Caballero Díaz <desarrollo@spicm.cfg.sld.cu>
2015-09-03 11:54:30 -0400

    fixed module default argumets issue
    - fixed module default argumets issue
    - code refactoring

--- modules/plugin_simple_seo/seo.py
+++ modules/plugin_simple_seo/seo.py
@@ -4,34 +4,39 @@ from gluon import *
 from gluon.contrib.ordereddict import OrderedDict
 
 
-def set_seo_meta(type="website", card="summary", title=current.request.application.replace('_', ' ').title(),
+def set_seo_meta(type="website", card="summary", title=None,
                  author=None, keywords=None, generator="Web2py Web Framework",
-                 url=URL(args=current.request.args, host=True),
-                 image=None, description=None, site_name=None,
+                 url=None, image=None, description=None, site_name=None,
                  locale=None, locale_alternate={}, creator=None,
                  site=None, label1=None, data1=None, label2=None, data2=None):
+    if not title:
+        title = current.request.application.replace('_', ' ').title()
+    if not url:
+        url = URL(args=current.request.args, host=True)
     set_meta(title, description, keywords, author, generator)
     set_open_graph(type, title, url, image, description, site_name, locale, locale_alternate)
     set_twitter_card(card, title, creator, site, label1, data1, label2, data2, image, description)
 
 
-def set_meta(title=current.request.application.replace('_', ' ').title(), description=None, keywords=None,
+def set_meta(title=None, description=None, keywords=None,
              author=None, generator="Web2py Web Framework"):
+    data = locals()
+    for name in ['title', 'description', 'keywords', 'author', 'generator']:
+        if data[name]:
+            current.response.meta[name] = data[name]
+    if not title:
+        title = current.request.application.replace('_', ' ').title()
     current.response.title = title
-    if description:
-        set_description_meta(description)
-    if keywords:
-        set_keywords_meta(keywords)
-    if author:
-        set_author_meta(author)
-    set_generator_meta(generator)
 
 
 
-def set_open_graph(type="website", title=current.response.title,
-                   url=URL(args=current.request.args, host=True),
-                   image=None, description=current.response.meta.description,
+def set_open_graph(type="website", title=None,
+                   url=None, image=None, description=None,
                    site_name=None, locale=None, locale_alternate={}):
+    if not title:
+        title = current.request.application.replace('_', ' ').title()
+    if not url:
+        url = URL(args=current.request.args, host=True)
     data = locals()
     for name in ['type', 'title', 'url', 'description', 'site_name', 'locale']:
         dict = OrderedDict()
@@ -43,10 +48,12 @@ def set_open_graph(type="website", title=current.response.title,
         set_og_image(image)
 
 
-def set_twitter_card(card="summary", title=current.response.title,
+def set_twitter_card(card="summary", title=None,
                      creator=None, site=None, label1=None,
                      data1=None, label2=None, data2=None,
-                     image=None, description=current.response.meta.description):
+                     image=None, description=None):
+    if not title:
+        title = current.request.application.replace('_', ' ').title()
     data = locals()
     for name in ['card', 'title', 'description', 'creator', 'site', 'label1', 'data1', 'label2', 'data2']:
         dict = OrderedDict()
@@ -58,26 +65,7 @@ def set_twitter_card(card="summary", title=current.response.title,
         set_tc_image(image)
 
 
-# Basic site meta
-
-
-def set_description_meta(description):
-    current.response.meta.description = description
-
-
-def set_keywords_meta(keywords):
-    current.response.meta.keywords = keywords
-
-
-def set_author_meta(author):
-    current.response.meta.author = author
-
-
-def set_generator_meta(generator):
-    current.response.meta.generator = generator
-
 # Open Graph meta
-
 
 
 def set_og_image(image):
