79796146838219c63ce30017dd96509b1d365a78
Carlos Cesar Caballero Díaz <desarrollo@spicm.cfg.sld.cu>
2015-09-03 01:09:38 -0400

    added data and label twitter cards
    - added data and label twitter cards
    - code refactoring

--- modules/plugin_simple_seo/seo.py
+++ modules/plugin_simple_seo/seo.py
@@ -1,16 +1,18 @@
 __author__ = 'cccaballero'
 
 from gluon import *
+from gluon.contrib.ordereddict import OrderedDict
 
 
-def set_seo_meta(data_type="website", card="summary", title=current.request.application.replace('_', ' ').title(),
+def set_seo_meta(type="website", card="summary", title=current.request.application.replace('_', ' ').title(),
                  author=None, keywords=None, generator="Web2py Web Framework",
-                 url=URL(args=current.request.args, vars=current.request.get_vars, host=True),
+                 url=URL(args=current.request.args, host=True),
                  image=None, description=None, site_name=None,
-                 locale=None, locale_alternate={}):
+                 locale=None, locale_alternate={}, creator=None,
+                 site=None, label1=None, data1=None, label2=None, data2=None):
     set_meta(title, description, keywords, author, generator)
-    set_open_graph(data_type, title, url, image, description, site_name, locale, locale_alternate)
-    set_twitter_card(card, title, url, image, description)
+    set_open_graph(type, title, url, image, description, site_name, locale, locale_alternate)
+    set_twitter_card(card, title, creator, site, label1, data1, label2, data2, image, description)
 
 
 def set_meta(title=current.request.application.replace('_', ' ').title(), description=None, keywords=None,
@@ -26,32 +28,34 @@ def set_meta(title=current.request.application.replace('_', ' ').title(), descri
 
 
 
-def set_open_graph(data_type="website", title=current.response.title,
-                   url=URL(args=current.request.args, vars=current.request.get_vars, host=True),
-                   image=None, description=current.response.meta.description, site_name=None, locale=None, locale_alternate={}):
-    set_og_type_meta(data_type)
-    set_og_title_meta(title)
-    set_og_url_meta(url)
+def set_open_graph(type="website", title=current.response.title,
+                   url=URL(args=current.request.args, host=True),
+                   image=None, description=current.response.meta.description,
+                   site_name=None, locale=None, locale_alternate={}):
+    data = locals()
+    for name in ['type', 'title', 'url', 'description', 'site_name', 'locale']:
+        dict = OrderedDict()
+        if data[name]:
+            dict['name'] = "og:"+name
+            dict['content'] = data[name]
+            current.response.meta['og_'+name] = dict
     if image:
         set_og_image(image)
-    if description:
-        set_og_description(description)
-    if site_name:
-        set_og_site_name(site_name)
-    if locale:
-        set_og_locale(locale)
-    set_og_locale_alternate(locale_alternate)
 
 
 def set_twitter_card(card="summary", title=current.response.title,
-                     url=URL(args=current.request.args, vars=current.request.get_vars, host=True),
+                     creator=None, site=None, label1=None,
+                     data1=None, label2=None, data2=None,
                      image=None, description=current.response.meta.description):
-    set_tc_card(card)
-    set_tc_title(title)
-    set_tc_url(url)
+    data = locals()
+    for name in ['card', 'title', 'description', 'creator', 'site', 'label1', 'data1', 'label2', 'data2']:
+        dict = OrderedDict()
+        if data[name]:
+            dict['property'] = "twitter:"+name
+            dict['content'] = data[name]
+            current.response.meta['tc_'+name] = dict
     if image:
         set_tc_image(image)
-    set_tc_description(description)
 
 
 # Basic site meta
@@ -75,63 +79,28 @@ def set_generator_meta(generator):
 # Open Graph meta
 
 
-def set_og_type_meta(data_type):
-    current.response.meta.og_type = {"property": "og:type", "content": data_type}
-
-
-def set_og_title_meta(title):
-    current.response.meta.og_title = {"property": "og:title", "content": title}
-
-
-def set_og_url_meta(url):
-    current.response.meta.og_url = {"property": "og:url", "content": url}
-
 
 def set_og_image(image):
     if isinstance(image, list):
         for count in range(len(image)):
-            current.response.meta['og_image_'+str(count)] = {"property": "og:image", "content": image[count]}
+            dict = OrderedDict()
+            dict['property'] = "og:image"
+            dict['content'] = image[count]
+            current.response.meta['og_image_'+str(count)] = dict
     else:
-        current.response.meta.og_image = {"property": "og:image", "content": image}
-
-
-def set_og_description(description):
-    current.response.meta.og_description = {"property": "og:description", "content": description}
-
-
-def set_og_site_name(site_name):
-    current.response.meta.og_site_name = {"property": "og:site_name", "content": site_name}
-
-
-def set_og_locale(locale):
-    current.response.meta.og_locale = {"property": "og:locale", "content": locale}
-
-
-def set_og_locale_alternate(locale_alternate):
-    for locale in locale_alternate:
-        current.response.meta["og_locale_"+locale] = {"property": "og:locale:alternate", "content": locale}
+        dict = OrderedDict()
+        dict['property'] = "og:image"
+        dict['content'] = image
+        current.response.meta.og_image = dict
 
 
 # Twitter Card meta
 
-def set_tc_card(card):
-    current.response.meta.tc_card = {"name": "twitter:card", "content": card}
-
-
-def set_tc_title(title):
-    current.response.meta.tc_title = {"name": "twitter:title", "content": title}
-
-
-def set_tc_url(url):
-    current.response.meta.tc_url = {"name": "twitter:url", "content": url}
-
-
 def set_tc_image(image):
+    dict = OrderedDict()
+    dict['name'] = "twitter:image"
     if isinstance(image, list):
-        current.response.meta.tc_image = {"name": "twitter:image", "content": image[0]}
+        dict['content'] = image[0]
     else:
-        current.response.meta.tc_image = {"name": "twitter:image", "content": image}
-
-
-def set_tc_description(description):
-    current.response.meta.tc_description = {"name": "twitter:description", "content": description}
+        dict['content'] = image
+    current.response.meta.tc_image = dict
